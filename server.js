1c2fb97b44141cf324ca502a022db4d14246ff2c
Kiara Johnson <johns.kia35@gmail.com>
2022-12-16 12:29:57 -0700

    New Route

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+// require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.urlencoded({ extended: false }));
+
 // Index Route
 
 
 // New
 app.get("/logs/new", (req, res) => {
-    res.send("New");
+    res.render("New");
   });
 
 // Create
 
 // Show Route
+
 
 
 // Mongo
@@ -21,3 +30,9 @@ app.get("/logs/new", (req, res) => {
 
 
 // Put Route
+
+//Port
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
--- views/New.jsx
+++ views/New.jsx
@@ -5,7 +5,15 @@ export default class New extends Component {
     return (
       <div>
         <h1>New Log</h1>
-        <form action="/logs" methods="POST"></form>
+        <form action="/logs" methods="POST">
+          Title: <input type="text" name="title" />
+          <br />
+          Entry: <input type="textarea" name="entry" />
+          <br />
+          Ship is broken: <input type="checkbox" name="shipIsBroken" />
+          <br />
+          <input type="submit" name="" value="Create Entry" />
+        </form>
       </div>
     );
   }
